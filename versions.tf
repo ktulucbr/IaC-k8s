a70fa63a567570c0613aadaa5038b27801d37811
stoulmonde <s.toulmonde@protonmail.com>
2022-06-15 11:56:10 +0000

    tfstate backend

--- versions.tf
+++ versions.tf
@@ -4,6 +4,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "2.66.0"
     }
+    backend "azurerm" {
+      resource_group_name  = "rg-tamopstf"
+      storage_account_name = "seppelfabius"
+      container_name       = "terraformstate"
+      key                  = "terraform.tfstate"
+    }
   }
 
   required_version = ">= 0.14"
