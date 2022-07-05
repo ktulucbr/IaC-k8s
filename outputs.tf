e5331e9d94130dfa4830c8f80abe74edb209b26d
Sebastien Toulmonde <s.toulmonde@protonmail.com>
2022-07-05 07:35:55 +0000

    added sensitive = true

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,7 @@ output "kubernetes_cluster_name" {
 output "acr_admin_password" {
   value = azurerm_container_registry.acr.admin_password
   description = "The object ID of the user"
+  sensitive = true
 }
 
 # output "host" {
